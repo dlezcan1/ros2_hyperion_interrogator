7df80b7bb780a809c393bd1b773cac86dc0225f0
Dimitri Lezcano <dlezcan1@jhu.edu>
2021-06-17 18:03:32 -0400

    Update of hyperion publisher

--- hyperion_interrogator/hyperion_talker.py
+++ hyperion_interrogator/hyperion_talker.py
@@ -12,16 +12,15 @@ from hyperion import AsyncHyperion
 class HyperionPublisher(Node):
     def __init__(self):
         super().__init__('HyperionPublisher')
-        self.signal_pub      = self.create_publisher(Float64MultiArray, 'sensor/raw', 10)
-        self.get_logger().info('Publishing signals.')
         
-        self.proc_signal_pub = self.create_publisher(Float64MultiArray, 'sensor/processed', 10)
-        self.get_logger().info('Publishing processed signals.')
+        # setup the publishers
+        self.instantiate_publishers()
     
         # Hyperion parameters
         self.ip_address = '10.0.0.5' # default value
         self.get_params()
         
+        # connect to Hyperion Interrogator
         self.connect()
         
     # __init__
@@ -36,6 +35,8 @@ class HyperionPublisher(Node):
         ''' Read in parameters for the Hyperion interrogator '''
         # Hyperion parameter names to get
         ip_param_name = 'interrogator/ip_address'
+        num_ch_param_name = 'num_channels'
+        num_aa_param_name = 'num_active_areas'
         
         # Hyperion IP address
         if self.has_parameter(ip_param_name):
@@ -44,12 +45,47 @@ class HyperionPublisher(Node):
         # if
         
         else:
-        self.get_logger().warning(("Interrogators IP address parameter,"
-                                    "'{}/interrogator/ip_address' not set."
-                                    " Using default IP address: {}").format(self.get_name(),self.ip_address))
+            self.get_logger().warning(("Interrogators IP address parameter, "
+                                       "'{}/{}' not set. Using default IP address: "
+                                       "{}").format(self.get_name(), ip_param_name, self.ip_address))
                                         
         # else
         
+        # Number of channels
+        try:
+            self.num_ch = self.get_paramater(num_ch_param_name).get_paramater_value()
+
+        # try
+        except:
+            self.get_logger().error(("Number of channels param not set, "
+                                     "'{}/{}'.").format(self.get_name(), num_ch_param_name))
+
+        # except
+        
+        # Number of active areas
+        try:
+            self.num_aa = self.get_paramater(num_aa_param_name).get_paramater_value()
+
+        # try
+        except:
+            self.get_logger().error(("Number of a param not set, "
+                                     "'{}/{}'.").format(self.get_name(), num_aa_param_name))
+
+        # except
+        
+        
     # get_params
+    
+    def instantiate_publishers(self):
+        ''' Method to instantiate the publishers for the class'''
+        # raw sensor data
+        self.signal_pub      = self.create_publisher(Float64MultiArray, 'sensor/raw', 10)
+        self.get_logger().info('Publishing signals.')
+        
+        # processed sensor data
+        self.proc_signal_pub = self.create_publisher(Float64MultiArray, 'sensor/processed', 10)
+        self.get_logger().info('Publishing processed signals.')
+        
+    # instantiate_publishers
     
 # class: HyperionPublisher
