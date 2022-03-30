62f3a1f85e1fbe2e2dd4b854fa4e25d01b39d21d
Dimitri Lezcano <dlezcan1@jh.edu>
2022-03-30 12:30:37 -0400

    updated launch files for optimization & numSamples launch arg

--- launch/hyperion_demo.launch.py
+++ launch/hyperion_demo.launch.py
@@ -1,37 +1,43 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
-from launch.substitutions import LaunchConfiguration, TextSubstitution
-from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
     ld = LaunchDescription()
-    
+
     # arguments
-    hyperion_ip_arg = DeclareLaunchArgument('ip',
-                                            default_value='10.0.0.55'
-                                            )
-    demo_num_chs_arg = DeclareLaunchArgument('numCH', default_value="3")
-    demo_num_aa_arg  = DeclareLaunchArgument('numAA', default_value="4")
-    
+    hyperion_ip_arg = DeclareLaunchArgument( 'ip',
+                                             default_value='10.0.0.55'
+                                             )
+    num_samples_arg = DeclareLaunchArgument( 'numSamples',
+                                             default_value='200'
+                                             )
+    demo_num_chs_arg = DeclareLaunchArgument( 'numCH', default_value="3" )
+    demo_num_aa_arg = DeclareLaunchArgument( 'numAA', default_value="4" )
+
     # Nodes
     hyperion_node = Node(
-                        package='hyperion_interrogator',
-                        namespace='needle',
-                        executable='hyperion_demo',
-                        output='screen',
-                        emulate_tty=True,
-                        parameters=[{
-                                    "interrogator.ip_address": LaunchConfiguration('ip'),
-                                    "demo.num_channels": LaunchConfiguration("numCH"),
-                                    "demo.num_active_areas": LaunchConfiguration("numAA")
-                                    }]
-                       )
+            package='hyperion_interrogator',
+            namespace='needle',
+            executable='hyperion_demo',
+            output='screen',
+            emulate_tty=True,
+            parameters=[ {
+                    "interrogator.ip_address": LaunchConfiguration( 'ip' ),
+                    "sensor.num_samples": LaunchConfiguration( 'numSamples' ),
+                    "demo.num_channels"      : LaunchConfiguration( "numCH" ),
+                    "demo.num_active_areas"  : LaunchConfiguration( "numAA" ),
+                    } ]
+            )
     # add to launch description
-    ld.add_action(hyperion_ip_arg)
-    ld.add_action(demo_num_chs_arg)
-    ld.add_action(demo_num_aa_arg)
-    ld.add_action(hyperion_node)
-       
+    ld.add_action( hyperion_ip_arg )
+    ld.add_action( num_samples_arg )
+    ld.add_action( demo_num_chs_arg )
+    ld.add_action( demo_num_aa_arg )
+    ld.add_action( hyperion_node )
+
     return ld
-     
- # generate_launch_description
+
+# generate_launch_description
--- launch/hyperion_talker.launch.py
+++ launch/hyperion_talker.launch.py
@@ -1,29 +1,35 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
-from launch.substitutions import LaunchConfiguration, TextSubstitution
-from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
     ld = LaunchDescription()
-    
+
     # arguments
-    hyperion_ip_arg = DeclareLaunchArgument('ip',
-                                            default_value='10.0.0.55'
-                                            )
-    
+    hyperion_ip_arg = DeclareLaunchArgument( 'ip',
+                                             default_value='10.0.0.55'
+                                             )
+    num_samples_arg = DeclareLaunchArgument( 'numSamples',
+                                             default_value='200'
+                                             )
+
     # Nodes
     hyperion_node = Node(
-                        package='hyperion_interrogator',
-                        namespace='needle',
-                        executable='hyperion_talker',
-                        parameters=[{
-                                    "interrogator.ip_address": LaunchConfiguration('ip')
-                                    }]
-                       )
+            package='hyperion_interrogator',
+            namespace='needle',
+            executable='hyperion_talker',
+            parameters=[ {
+                    "interrogator.ip_address": LaunchConfiguration( 'ip' ),
+                    "sensor.num_samples"     : LaunchConfiguration( 'numSamples' ),
+                    } ]
+            )
     # add to launch description
-    ld.add_action(hyperion_ip_arg)
-    ld.add_action(hyperion_node)
-       
+    ld.add_action( hyperion_ip_arg )
+    ld.add_action( num_samples_arg )
+    ld.add_action( hyperion_node )
+
     return ld
-     
- # generate_launch_description
+
+# generate_launch_description
